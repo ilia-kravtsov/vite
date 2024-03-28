8dbf89518782f2e0984f03f1772659272ef23795
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-03-28 19:33:04 +0400

    sass was added

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './src',
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
   build: {
-    outDir: '../build'
-  }
-})
+    outDir: '../build',
+    emptyOutDir: true,
+  },
+});
