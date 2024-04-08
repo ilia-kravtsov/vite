e0b562d43c6aad04f6097dbcde6f459c1680590f
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-04-08 21:58:50 +0400

    contact section is created

---
+++ src/js/contact.js
@@ -0,0 +1,11 @@
+import '../assets/scss/main.scss';
+import '../assets/scss/contact.scss';
+
+// components
+import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
+import { useFooterAccordeon } from './components/footer-accordeon';
+
+useTheme();
+useBurger();
+useFooterAccordeon();
