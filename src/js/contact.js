1aec2c145c2edc17ea51c61266069478720075c0
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-04-10 16:30:52 +0400

    partners section on the contact page fixing

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,17 +1,11 @@
 // import '../assets/scss/main.scss';
 // import '../assets/scss/contact.scss';
-import '/scss/main.scss';
 import '/scss/contact.scss';
+import '/scss/main.scss';
 
 // components
-import { useTheme } from './components/theme';
-import { useBurger } from './components/burger';
 import { usePhone } from './components/contact/phone';
 import { useForm } from './components/contact/form';
-import { useFooterAccordeon } from './components/footer-accordeon';
 
-useTheme();
-useBurger();
 usePhone();
 useForm();
-useFooterAccordeon();
