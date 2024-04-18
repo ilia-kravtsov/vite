d4d848611826915a1c2953680469a874a4e392b0
ilia-kravtsov <kravtsov.i.s@mail.ru>
2024-04-18 20:25:56 +0300

    activity section is created

---
+++ src/js/activity-updates.js
@@ -0,0 +1,7 @@
+import '/scss/main.scss';
+import '/scss/activity-updates.scss';
+
+// components
+import { useActivityHeroSwiper } from './components/activity-updates/hero';
+
+useActivityHeroSwiper();
---
+++ src/js/components/activity-updates/hero.js
@@ -0,0 +1,33 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+export const useActivityHeroSwiper = () => {
+  new Swiper('.activity-updates__hero-swiper', {
+    modules: [Navigation],
+    direction: 'horizontal',
+    slidesPerView: 2.5,
+    spaceBetween: 32,
+    loop: true,
+    pagination: {
+      el: '.activity-updates__hero-swiper-pagination',
+    },
+    scrollbar: {
+      el: '.activity-updates__hero-swiper-scrollbar',
+    },
+
+    // breakpoints: {
+    //   993: {
+    //     slidesPerView: 2.5,
+    //   },
+    //   767: {
+    //     slidesPerView: 2,
+    //   },
+    //   577: {
+    //     slidesPerView: 2,
+    //   },
+    //   0: {
+    //     slidesPerView: 1,
+    //   },
+    // },
+  });
+};
